c5846ce617fda65375da5128da1e0cf51d1e4c01
Kinga Kisielewska <kkisielewska9@gmail.com>
2025-02-21 22:18:18 +0000

    further EDIT of rps

--- rps.py
+++ rps.py
@@ -1,18 +1,43 @@
+import random
 # Prompt user input
+
+def result(comp_choice, human_choice):
+    """
+    This function defines the result of the game through comparing the computer and human choice
+    :param comp_choice: random int chosen by the computer from 0-2 range
+    :param human_choice: input- R, P, S
+    """
+    if human_choice == comp_choice:
+        return "tie"
+    elif (human_choice == "rock" and comp_choice == "scissors") or \
+            (human_choice == "scissors" and comp_choice == "paper") or \
+            (human_choice == "paper" and comp_choice == "rock"):
+        return "win"
+    else:
+        return "lose"
+
 print("Welcome to the Rock, Paper, Scissors game.")
 # create a loop for the game to be able to run multiple times
 while True:
-    human_choice = input("Chose R, P or S:")
-    # create a variable to store the human's choice
-    if human_choice == "R":
-        human_choice = "Rock"
-    elif human_choice == "P":
-        human_choice = "Paper"
-    else:
-        human_choice = "Scissors"
+    while True:
+        human_choice = input("Chose R, P or S:")
+    # create a variable to store the human's choice, break the loop and move on if condition met
+        if human_choice == "R":
+            human_choice = "Rock"
+            break
+        elif human_choice == "P":
+            human_choice = "Paper"
+            break
+        elif human_choice == "S":
+            human_choice = "Scissors"
+            break
+        else:
+            print("Invalid input, try again")
+
+    #     loop if input not one of the three options
     # create a codnition and convert the single letter string into a full answer
     # generate random value
-    import random
+
     comp_choice = random.randrange(0,2)
     # generate random in from the tange above through importing the ranfom module
     # conversion of the computer choice/random integer into a string which corresponds to r/p/s
@@ -22,28 +47,21 @@ while True:
         comp_choice = "Paper"
     else:
         comp_choice = "Scissors"
+    # interact with the user through string formatting to let them know computer choice
     print(f"You chose {human_choice} and the computer chose {comp_choice}")
     print("Hence...")
-    # interact with the user through string formatting to let them know computer choice
-    def result(comp_choice, human_choice):
-        """
-        This function defines the result of the game through comparing the computer and human choice
-        :param comp_choice: random int chosen by the computer from 0-2 range
-        :param human_choice: input- R, P, S
 
-        """
-        if human_choice == comp_choice:
+    #call function and give options which are contrasted against returned result
+    r = result(comp_choice, human_choice)
+    if r == "tie":
             print("It's a tie!")
-        elif (human_choice == "rock" and comp_choice == "scissors") or \
-             (human_choice == "scissors" and comp_choice == "paper") or \
-             (human_choice == "paper" and comp_choice == "rock"):
-           print("You win!")
-        else:
-           print("You lose!")
-    # function defined above - could this be done not using print?
-    result(comp_choice, human_choice)
-    # call function
+    elif r == "win":
+            print("Wooo, you win!")
+    else:
+            print ("Sorry, you lose...")
+
+
     question = input("Want to play again? Y/N")
-    if question == "No":
+    if question == "N":
         break
-   #give user the option to play again
+   #give user the option to play again or leave
